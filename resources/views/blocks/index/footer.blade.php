3eff225f2d921ee43bb5f6e0edfc2465e5ec9bbd
devilandcode <devilandcode@proton.me>
2023-11-11 22:00:42 +0300

    Add styles and index templates

---
+++ resources/views/blocks/index/footer.blade.php
@@ -0,0 +1,1 @@
+<footer class="footer">&copy; 2023</footer>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.main.content');
 });
 
 Route::get('/news', function () {
