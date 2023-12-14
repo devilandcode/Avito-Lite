da3e84018d0c6af4b640b095d6a527a24bd48e43
devilandcode <devilandcode@proton.me>
2023-12-14 19:37:18 +0300

    Add link component index template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,23 +14,50 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
+    <body>
+        <div class="h-screen bg-white">
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
+                {{ $header }}
             @endif
-
+            <!-- Page Nav -->
+            @if (isset($nav))
+                {{ $nav }}
+            @endif
+            <!-- Page Section -->
+            @if (isset($section))
+                {{ $section }}
+            @endif
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
+            <!-- Page Footer -->
+            <footer class="bg-black mt-2">
+                <div class="container max-w-4xl mx-auto">
+                    <div class="flex justify-between text-sm py-2">
+                        <div class="">
+                            <span class="text-white">2023 &copy; Avito LLC</span>
+                        </div>
+                        <div class="">
+                            <ul class="flex space-x-2 text-white">
+                                <li>
+                                    <x-footer.link>Information</x-footer.link>
+                                </li>
+                                <li>
+                                    <x-footer.link>Career</x-footer.link>
+                                </li>
+                                <li>
+                                    <x-footer.link>Press</x-footer.link>
+                                </li>
+                                <li>
+                                    <x-footer.link>About</x-footer.link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
     </body>
 </html>
